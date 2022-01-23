5cbb1f7cd24fdbd9d3860aba6d84068cea84e2a4
GnaixEuy <sgghcj@live.com>
2022-01-23 10:46:46 +0800

    first commit

---
+++ src/main/java/cn/limitless/cathatmusic/CatHatMusicApplication.java
@@ -0,0 +1,13 @@
+package cn.limitless.cathatmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatHatMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatHatMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/limitless/cathatmusic/CatHatMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.limitless.cathatmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatHatMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
