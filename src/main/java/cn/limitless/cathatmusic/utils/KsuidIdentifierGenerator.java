065f4c3ebce53367f63e86b8f4a8b890fb0265e3
GnaixEuy <sgghcj@live.com>
2022-01-25 23:19:54 +0800

    :sparkles: 自增id 由雪花算法+uuid 迁移至 ksuid的算法

--- src/main/java/cn/limitless/cathatmusic/utils/KsuidIdentifierGenerator.java
+++ src/main/java/cn/limitless/cathatmusic/utils/KsuidIdentifierGenerator.java
@@ -1,5 +1,10 @@
 package cn.limitless.cathatmusic.utils;
 
+import com.baomidou.mybatisplus.core.incrementer.DefaultIdentifierGenerator;
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import com.github.ksuid.KsuidGenerator;
+import org.springframework.stereotype.Component;
+
 /**
  * <img src="http://blog.GnaixEuy.cn/wp-content/uploads/2021/08/bug.jpeg"/>
  *
@@ -7,15 +12,16 @@ package cn.limitless.cathatmusic.utils;
  * @date 2022/1/24
  * @see <a href='https://github.com/GnaixEuy'> GnaixEuy的GitHub </a>
  */
-//public class KsuidIdentifierGenerator implements IdentifierGenerator {
-//
-//	@Override
-//	public Number nextId(Object entity) {
-//		return null;
-//	}
-//
-//	@Override
-//	public String nextUUID(Object entity) {
-//		return KsuidGenerator.generate();
-//	}
-//}
+@Component
+public class KsuidIdentifierGenerator implements IdentifierGenerator {
+
+	@Override
+	public Number nextId(Object entity) {
+		return new DefaultIdentifierGenerator().nextId(entity);
+	}
+
+	@Override
+	public String nextUUID(Object entity) {
+		return KsuidGenerator.generate();
+	}
+}
